d3f71b13b033b0abd8efd532c5df88628d54434b
mback2k <mback2k@googlemail.com>
2011-09-28 00:19:40 -0700

    Added XML resource handler

--- src/de/uxnr/tsoexpert/TSOExpert.java
+++ src/de/uxnr/tsoexpert/TSOExpert.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import de.uxnr.proxy.Proxy;
 import de.uxnr.tsoexpert.proxy.GameHandler;
 import de.uxnr.tsoexpert.proxy.StaticHandler;
+import de.uxnr.tsoexpert.resource.XMLHandler;
 
 public class TSOExpert {
 	public static void main(String[] args) throws IOException, InterruptedException {
+		StaticHandler.addResourceHandler(".*\\.xml", new XMLHandler());
+		
 		Proxy proxy = new Proxy(8000);
 		proxy.addHostHandler("game(\\d*).diesiedleronline.de", new GameHandler());
 		proxy.addHostHandler("static(\\d*).cdn.ubi.com", new StaticHandler());
---
+++ src/de/uxnr/tsoexpert/resource/XMLHandler.java
@@ -0,0 +1,44 @@
+package de.uxnr.tsoexpert.resource;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLHandler implements ResourceHandler {
+	private Document document = null;
+	
+	@Override
+	public void handleResource(File file) throws IOException {
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			this.document = db.parse(file);
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+		
+		System.out.println(file.getName());
+		
+		NodeList nodes = this.document.getElementsByTagName("Building");
+		
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			NamedNodeMap attrs = node.getAttributes();
+			System.out.println(attrs.getNamedItem("name"));
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		File file = new File("res/GFX/gfx_settings.xml");
+		
+		XMLHandler xh = new XMLHandler();
+		xh.handleResource(file);
+	}
+}
