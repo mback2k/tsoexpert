a9588b857890435e481504acda39717864d7d239
sewe42 <informaticus1@googlemail.com>
2011-10-01 14:17:20 -0700

    Added ChatHandler to the main proxy.

--- src/de/uxnr/tsoexpert/TSOExpert.java
+++ src/de/uxnr/tsoexpert/TSOExpert.java
@@ -3,6 +3,7 @@ package de.uxnr.tsoexpert;
 import java.io.IOException;
 
 import de.uxnr.proxy.Proxy;
+import de.uxnr.tsoexpert.proxy.ChatHandler;
 import de.uxnr.tsoexpert.proxy.GameHandler;
 import de.uxnr.tsoexpert.proxy.StaticHandler;
 import de.uxnr.tsoexpert.resource.XMLHandler;
@@ -14,6 +15,7 @@ public class TSOExpert {
 		Proxy proxy = new Proxy(8000);
 		proxy.addHostHandler("game(\\d*).diesiedleronline.de", new GameHandler());
 		proxy.addHostHandler("static(\\d*).cdn.ubi.com", new StaticHandler());
+		proxy.addHostHandler("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})", new ChatHandler());
 		proxy.run();
 	}
 }
