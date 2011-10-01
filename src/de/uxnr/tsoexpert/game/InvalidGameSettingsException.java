56fdc34d5214f39e987ae5c8a7d054aaba2893a5
sewe42 <informaticus1@googlemail.com>
2011-10-01 14:28:40 -0700

    Added invalide game settings exception

---
+++ src/de/uxnr/tsoexpert/game/InvalidGameSettingsException.java
@@ -0,0 +1,11 @@
+package de.uxnr.tsoexpert.game;
+
+public class InvalidGameSettingsException extends Exception {
+
+	private static final long serialVersionUID = -6037746332043671202L;
+
+	public InvalidGameSettingsException(String string) {
+		super(string);
+	}
+
+}
