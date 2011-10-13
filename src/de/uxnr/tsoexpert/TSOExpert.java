73d95f1a9bf1b103cc38d905708fa08dc0687354
mback2k <mback2k@googlemail.com>
2011-10-13 01:49:20 -0700

    Updated GameHandler to use new communication classes and support DataHandlers

--- src/de/uxnr/tsoexpert/TSOExpert.java
+++ src/de/uxnr/tsoexpert/TSOExpert.java
@@ -3,12 +3,17 @@ package de.uxnr.tsoexpert;
 import java.io.IOException;
 
 import de.uxnr.proxy.Proxy;
+import de.uxnr.tsoexpert.game.PlayerListHandler;
+import de.uxnr.tsoexpert.game.ZoneHandler;
 import de.uxnr.tsoexpert.proxy.GameHandler;
 import de.uxnr.tsoexpert.proxy.StaticHandler;
 import de.uxnr.tsoexpert.resource.XMLHandler;
 
 public class TSOExpert {
 	public static void main(String[] args) throws IOException, InterruptedException {
+		GameHandler.addDataHandler(1001, new ZoneHandler());
+		GameHandler.addDataHandler(1014, new PlayerListHandler());
+
 		StaticHandler.addResourceHandler(".*\\.xml", new XMLHandler());
 
 		Proxy proxy = new Proxy(8000);
