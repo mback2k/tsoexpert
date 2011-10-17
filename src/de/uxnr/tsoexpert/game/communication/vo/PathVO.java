dfa8d6ac71b0c879c16e44bb3b1c69246bd55776
mback2k <mback2k@googlemail.com>
2011-10-17 15:12:16 -0700

    Changed Integer type in PathVO

--- src/de/uxnr/tsoexpert/game/communication/vo/PathVO.java
+++ src/de/uxnr/tsoexpert/game/communication/vo/PathVO.java
@@ -3,6 +3,7 @@ package de.uxnr.tsoexpert.game.communication.vo;
 import java.util.List;
 
 import de.uxnr.amf.v3.AMF3_Object;
+import de.uxnr.amf.v3.type.Integer;
 
 public class PathVO extends AMF3_Object {
 	private List<Integer> mPath;
