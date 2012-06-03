6eface769632d4fbb00c245a1e668218ccd34a6f
mback2k <mback2k@googlemail.com>
2012-06-03 16:15:21 +0200

    Updated rendering hints

--- src/de/uxnr/tsoexpert/ui/map/ZoneMap.java
+++ src/de/uxnr/tsoexpert/ui/map/ZoneMap.java
@@ -178,7 +178,11 @@ public class ZoneMap {
 		Rectangle clip = new Rectangle(this.offset);
 		Graphics2D g = this.doubleBuffer.createGraphics();
 
+//		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+//		g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//		g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
 		g.setBackground(graphics.getBackground());
 		g.setColor(graphics.getColor());
 		g.setFont(graphics.getFont());
