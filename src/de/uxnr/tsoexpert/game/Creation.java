e9fdea1d33f7689fef76c3372011517c5a938fe2
Sewe42 <informaticus1@googlemail.com>
2012-06-03 02:16:45 +0200

    Made Creation implement Parsable

--- src/de/uxnr/tsoexpert/game/Creation.java
+++ src/de/uxnr/tsoexpert/game/Creation.java
@@ -3,7 +3,9 @@ package de.uxnr.tsoexpert.game;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Creation {
+import org.w3c.dom.Node;
+
+public class Creation implements Parsable{
 
 	private static Map<Integer, Creation> CreationMap = new HashMap<Integer, Creation>();
 
@@ -22,6 +24,12 @@ public class Creation {
 			c = new Creation(id);
 		}
 		return c;
+	}
+
+	@Override
+	public void parse(Node node) throws InvalidGameSettingsException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
